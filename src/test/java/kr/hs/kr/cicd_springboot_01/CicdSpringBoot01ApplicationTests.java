12361a14a2e045128c75c607d81968c319f6a254
lyhtll <a62346986@gmail.com>
2026-06-24 09:15:36 +0900

    ignore

---
+++ src/test/java/kr/hs/kr/cicd_springboot_01/CicdSpringBoot01ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.kr.cicd_springboot_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdSpringBoot01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
